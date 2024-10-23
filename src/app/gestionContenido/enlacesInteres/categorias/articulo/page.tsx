32a7eedf589df6cfb3ee8d5d7c1c975837e55b50
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-10-23 15:03:12 -0500

    feat: Corregir nombres en archivos en Enlaces de interes

--- src/app/gestionContenido/enlacesInteres/categorias/articulo/page.tsx
+++ src/app/gestionContenido/enlacesInteres/categorias/articulo/page.tsx
@@ -2,14 +2,18 @@ import EntradaArticulo from "@/app/gestionContenido/enlacesInteres/categorias/ar
 import PieDePagina from "@/components/ui/footer/PieDePagina";
 import { TopMenu } from "@/components/ui/top-menu/TopMenu";
 
-export default function EI_Categorias_Articulo() {
-    return (
-        <div>
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
             {/* Header */}
             <TopMenu />
             <h1 className='h1-bold txtcolor-primary' style={{ padding: '21px 0px 0px 55px' }}>Categoria</h1>
             <EntradaArticulo />
             <PieDePagina />
         </div>
-    );
-} 
+  )
+}
+
+export default page
