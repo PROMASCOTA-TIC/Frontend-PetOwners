9f5a6d99155405f97238e230ba4e836cc5dffd33
jackson-ramon <jackson.ramon@epn.edu.ec>
2024-10-27 04:04:50 -0500

    feat: Prueba para comprobar el funcionamiento del botón de Payphone

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,8 +1,40 @@
+"use client";
+import { Button } from "@mui/material";
 
 export default function() {
+  const handlePay = async () => {
+    const parametros = {
+      amount: 840,
+      amountWithoutTax: 840,
+      clientTransactionId: "Pago-test-008",
+      responseUrl: "http://localhost:3000/pay/response",
+      cancellationUrl: "http://localhost:3000/pay/response",
+    };
+
+    try {
+      const response = await fetch("https://pay.payphonetodoesposible.com/api/button/Prepare", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer PAYPHONE_TOKEN",
+      },
+      body: JSON.stringify(parametros),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      window.location.href = data.payWithCard
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Cart Page</h1>
+      <Button variant="contained" color="primary" onClick={handlePay} >
+        Pagar
+      </Button>
     </div>
   );
 }
