1b4136e640d2520abe95bddf0957658f63124463
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-11-08 15:32:49 -0500

    feat: Corregir pagina para mostrar el formulario de envio de enlaces de interes

--- src/app/(shop)/gestion-contenido/enlaces-interes/formulario/page.tsx
+++ src/app/(shop)/gestion-contenido/enlaces-interes/formulario/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { TopMenu } from '@/components/ui/top-menu/TopMenu'
-import PieDePagina from '@/components/ui/footer/PieDePagina'
 import Form_EnviarEnlace from './Form_EnviarEnlace'
 
 const page = () => {
   return (
     <div>
-      <TopMenu />
       <Form_EnviarEnlace />
-      <PieDePagina />
     </div>
   )
 }
